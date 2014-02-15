29765027c96ddcc557eeb0ba06ac56f18862d553
Konrad Kuźnicki <konrad@kuznicki.me>
2014-02-15 16:41:15 +0100

    Basic commands work throught shell but with errors
    	available: play, pause, toggle, next, prev

--- mpc_compiled.js
+++ mpc_compiled.js
@@ -3,16 +3,22 @@ goog.provide('lt.plugins.mpc');
 goog.require('cljs.core');
 goog.require('lt.objs.command');
 goog.require('lt.objs.command');
+goog.require('lt.objs.proc');
+goog.require('lt.objs.proc');
 goog.require('lt.objs.tabs');
 goog.require('lt.objs.tabs');
 goog.require('lt.object');
 goog.require('lt.object');
-lt.plugins.mpc.config = new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"mpclib","mpclib",4240972503),"mpd",new cljs.core.Keyword(null,"connection","connection",4533219920),new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"host","host",1017112858),"localhost",new cljs.core.Keyword(null,"port","port",1017351155),6600], null)], null);
-lt.plugins.mpc.get_js_lib = (function get_js_lib(path){return require(new cljs.core.Keyword(null,"mpclib","mpclib",4240972503).cljs$core$IFn$_invoke$arity$1(lt.plugins.mpc.config));
+/**
+* Pass commands to shell mpc
+*/
+lt.plugins.mpc.shell_mpc = (function shell_mpc(cmd){return cljs.core.partial.call(null,lt.objs.proc.exec,new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"command","command",1964298941),"mpc",new cljs.core.Keyword(null,"args","args",1016906831),[cmd]], null));
 });
-lt.plugins.mpc.mpclib = (function mpclib(){return lt.plugins.mpc.get_js_lib.call(null,new cljs.core.Keyword(null,"mpclib","mpclib",4240972503).cljs$core$IFn$_invoke$arity$1(lt.plugins.mpc.config));
-});
-lt.objs.command.command.call(null,new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"command","command",1964298941),new cljs.core.Keyword("lt.plugins.mpc","mpc-play","lt.plugins.mpc/mpc-play",2898949298),new cljs.core.Keyword(null,"desc","desc",1016984067),"mpc: play",new cljs.core.Keyword(null,"exec","exec",1017031683),console.log("asdf")], null));
+lt.objs.command.command.call(null,new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"command","command",1964298941),new cljs.core.Keyword("lt.plugins.mpc","mpc-play","lt.plugins.mpc/mpc-play",2898949298),new cljs.core.Keyword(null,"desc","desc",1016984067),"mpc: play",new cljs.core.Keyword(null,"exec","exec",1017031683),lt.plugins.mpc.shell_mpc.call(null,"play")], null));
+lt.objs.command.command.call(null,new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"command","command",1964298941),new cljs.core.Keyword("lt.plugins.mpc","mpc-pause","lt.plugins.mpc/mpc-pause",1463870314),new cljs.core.Keyword(null,"desc","desc",1016984067),"mpc: pause",new cljs.core.Keyword(null,"exec","exec",1017031683),lt.plugins.mpc.shell_mpc.call(null,"pause")], null));
+lt.objs.command.command.call(null,new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"command","command",1964298941),new cljs.core.Keyword("lt.plugins.mpc","mpc-toggle","lt.plugins.mpc/mpc-toggle",2182198482),new cljs.core.Keyword(null,"desc","desc",1016984067),"mpc: toggle",new cljs.core.Keyword(null,"exec","exec",1017031683),lt.plugins.mpc.shell_mpc.call(null,"toggle")], null));
+lt.objs.command.command.call(null,new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"command","command",1964298941),new cljs.core.Keyword("lt.plugins.mpc","mpc-next","lt.plugins.mpc/mpc-next",2899014641),new cljs.core.Keyword(null,"desc","desc",1016984067),"mpc: next",new cljs.core.Keyword(null,"exec","exec",1017031683),lt.plugins.mpc.shell_mpc.call(null,"next")], null));
+lt.objs.command.command.call(null,new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"command","command",1964298941),new cljs.core.Keyword("lt.plugins.mpc","mpc-prev","lt.plugins.mpc/mpc-prev",2898938801),new cljs.core.Keyword(null,"desc","desc",1016984067),"mpc: prev",new cljs.core.Keyword(null,"exec","exec",1017031683),lt.plugins.mpc.shell_mpc.call(null,"prev")], null));
 }
 
 //# sourceMappingURL=mpc_compiled.js.map
--- src/lt/plugins/mpc.cljs
+++ src/lt/plugins/mpc.cljs
@@ -1,29 +1,35 @@
 (ns lt.plugins.mpc
   (:require [lt.object :as object]
             [lt.objs.tabs :as tabs]
+            [lt.objs.proc :as proc]
             [lt.objs.command :as cmd])
   (:require-macros [lt.macros :refer [defui behavior]]))
 
 
-(def config
-  {:mpclib "mpd"
-   :connection {:host "localhost"
-                :port  6600}})
+(defn shell-mpc
+  "Pass commands to shell mpc"
+  [cmd]
+  (partial proc/exec
+    {:command "mpc" :args #js [cmd]}))
 
-
-(defn get-js-lib [path]
-  (js/require (:mpclib config)))
-
-
-(defn mpclib []
-  (get-js-lib (:mpclib config)))
-
-; (defn )
-
-
-; (.log js/console (.cmd (mpclib)))
 
 
 (cmd/command {:command ::mpc-play
               :desc "mpc: play"
-              :exec (.log js/console "asdf")})
+              :exec (shell-mpc "play")})
+
+(cmd/command {:command ::mpc-pause
+              :desc "mpc: pause"
+              :exec (shell-mpc "pause")})
+
+(cmd/command {:command ::mpc-toggle
+              :desc "mpc: toggle"
+              :exec (shell-mpc "toggle")})
+
+(cmd/command {:command ::mpc-next
+              :desc "mpc: next"
+              :exec (shell-mpc "next")})
+
+(cmd/command {:command ::mpc-prev
+              :desc "mpc: prev"
+              :exec (shell-mpc "prev")})
