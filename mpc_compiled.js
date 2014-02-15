eee08efcb225912243459cc82c64af4772801eb7
Konrad Kuźnicki <konrad@kuznicki.me>
2014-02-15 19:27:13 +0100

    behavior based mpc init with basic settings

--- mpc_compiled.js
+++ mpc_compiled.js
@@ -1,29 +1,36 @@
 if(!lt.util.load.provided_QMARK_('lt.plugins.mpc')) {
 goog.provide('lt.plugins.mpc');
 goog.require('cljs.core');
-goog.require('lt.objs.command');
-goog.require('lt.objs.command');
-goog.require('lt.objs.proc');
-goog.require('lt.objs.proc');
 goog.require('lt.objs.tabs');
-goog.require('lt.objs.tabs');
+goog.require('lt.objs.notifos');
+goog.require('lt.objs.proc');
+goog.require('lt.objs.notifos');
+goog.require('lt.objs.command');
 goog.require('lt.object');
 goog.require('lt.object');
+goog.require('lt.objs.proc');
+goog.require('lt.objs.tabs');
+goog.require('lt.objs.command');
+lt.plugins.mpc.__BEH__shell_mpc__DOT__out = (function __BEH__shell_mpc__DOT__out(obj,data){return lt.objs.notifos.set_msg_BANG_.call(null,data.toString());
+});
+lt.object.behavior_STAR_.call(null,new cljs.core.Keyword("lt.plugins.mpc","shell-mpc.out","lt.plugins.mpc/shell-mpc.out",2280790932),new cljs.core.Keyword(null,"reaction","reaction",4441361819),lt.plugins.mpc.__BEH__shell_mpc__DOT__out,new cljs.core.Keyword(null,"desc","desc",1016984067),"When mpc command is executed, show its out",new cljs.core.Keyword(null,"triggers","triggers",2516997421),new cljs.core.PersistentHashSet(null, new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"proc.out","proc.out",4302083112),null], null), null));
+lt.plugins.mpc.shell_mpc_out = lt.object.create.call(null,lt.object.object_STAR_.call(null,new cljs.core.Keyword("lt.plugins.mpc","shell-mpc-out","lt.plugins.mpc/shell-mpc-out",2280826605),new cljs.core.Keyword(null,"tags","tags",1017456523),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"shell-mpc-out","shell-mpc-out",1758412790)], null),new cljs.core.Keyword(null,"behaviors","behaviors",607554515),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword("lt.plugins.mpc","shell-mpc.out","lt.plugins.mpc/shell-mpc.out",2280790932)], null)));
 /**
 * Pass commands to shell mpc
 * @param {...*} var_args
 */
 lt.plugins.mpc.shell_mpc = (function() { 
-var shell_mpc__delegate = function (cmds){return cljs.core.partial.call(null,lt.objs.proc.exec,new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"command","command",1964298941),"mpc",new cljs.core.Keyword(null,"args","args",1016906831),cmds], null));
+var shell_mpc__delegate = function (mpc_path,cmds){return cljs.core.partial.call(null,lt.objs.proc.exec,new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"command","command",1964298941),mpc_path,new cljs.core.Keyword(null,"args","args",1016906831),cmds,new cljs.core.Keyword(null,"obj","obj",1014014057),lt.plugins.mpc.shell_mpc_out], null));
 };
-var shell_mpc = function (var_args){
-var cmds = null;if (arguments.length > 0) {
-  cmds = cljs.core.array_seq(Array.prototype.slice.call(arguments, 0),0);} 
-return shell_mpc__delegate.call(this,cmds);};
-shell_mpc.cljs$lang$maxFixedArity = 0;
-shell_mpc.cljs$lang$applyTo = (function (arglist__8402){
-var cmds = cljs.core.seq(arglist__8402);
-return shell_mpc__delegate(cmds);
+var shell_mpc = function (mpc_path,var_args){
+var cmds = null;if (arguments.length > 1) {
+  cmds = cljs.core.array_seq(Array.prototype.slice.call(arguments, 1),0);} 
+return shell_mpc__delegate.call(this,mpc_path,cmds);};
+shell_mpc.cljs$lang$maxFixedArity = 1;
+shell_mpc.cljs$lang$applyTo = (function (arglist__8481){
+var mpc_path = cljs.core.first(arglist__8481);
+var cmds = cljs.core.rest(arglist__8481);
+return shell_mpc__delegate(mpc_path,cmds);
 });
 shell_mpc.cljs$core$IFn$_invoke$arity$variadic = shell_mpc__delegate;
 return shell_mpc;
@@ -32,52 +39,52 @@ return shell_mpc;
 /**
 * Make local keyword from string
 */
-lt.plugins.mpc.mk_key = (function mk_key(n){return cljs.core.keyword.call(null,"lt.plugins.mpc",[cljs.core.str("mpc-"),cljs.core.str(n)].join(''));
+lt.plugins.mpc.mk_key = (function mk_key(n){return cljs.core.keyword.call(null,[cljs.core.str(lt.plugins.mpc._STAR_ns_STAR_)].join(''),[cljs.core.str("mpc-"),cljs.core.str(n)].join(''));
 });
 /**
-* Make LT commad that is visible
+* Make LT commad that is visible for LT
 */
-lt.plugins.mpc.mk_cmd = (function mk_cmd(kywrd){var str_kywrd = cljs.core.name.call(null,kywrd);var new_kywrd = lt.plugins.mpc.mk_key.call(null,str_kywrd);var desc = [cljs.core.str("mpc: "),cljs.core.str(str_kywrd)].join('');var shell = lt.plugins.mpc.shell_mpc.call(null,str_kywrd);return lt.objs.command.command.call(null,new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"command","command",1964298941),new_kywrd,new cljs.core.Keyword(null,"desc","desc",1016984067),desc,new cljs.core.Keyword(null,"exec","exec",1017031683),shell], null));
+lt.plugins.mpc.mk_cmd = (function mk_cmd(mpc_path,kywrd){var str_kywrd = cljs.core.name.call(null,kywrd);var new_kywrd = lt.plugins.mpc.mk_key.call(null,str_kywrd);var desc = [cljs.core.str("mpc: "),cljs.core.str(str_kywrd)].join('');var shell = lt.plugins.mpc.shell_mpc.call(null,mpc_path,str_kywrd);return lt.objs.command.command.call(null,new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"command","command",1964298941),new_kywrd,new cljs.core.Keyword(null,"desc","desc",1016984067),desc,new cljs.core.Keyword(null,"exec","exec",1017031683),shell], null));
 });
-lt.plugins.mpc.main_ = (function main_(){var seq__8398 = cljs.core.seq.call(null,new cljs.core.PersistentVector(null, 6, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"play","play",1017347750),new cljs.core.Keyword(null,"pause","pause",1120344424),new cljs.core.Keyword(null,"stop","stop",1017445236),new cljs.core.Keyword(null,"toggle","toggle",4440567494),new cljs.core.Keyword(null,"next","next",1017282149),new cljs.core.Keyword(null,"prev","prev",1017353637)], null));var chunk__8399 = null;var count__8400 = 0;var i__8401 = 0;while(true){
-if((i__8401 < count__8400))
-{var kywrd = cljs.core._nth.call(null,chunk__8399,i__8401);lt.plugins.mpc.mk_cmd.call(null,kywrd);
+lt.plugins.mpc.__BEH__mpc_init = (function __BEH__mpc_init(this$,mpc_path,commands){var seq__8477 = cljs.core.seq.call(null,commands);var chunk__8478 = null;var count__8479 = 0;var i__8480 = 0;while(true){
+if((i__8480 < count__8479))
+{var kywrd = cljs.core._nth.call(null,chunk__8478,i__8480);lt.plugins.mpc.mk_cmd.call(null,mpc_path,kywrd);
 {
-var G__8403 = seq__8398;
-var G__8404 = chunk__8399;
-var G__8405 = count__8400;
-var G__8406 = (i__8401 + 1);
-seq__8398 = G__8403;
-chunk__8399 = G__8404;
-count__8400 = G__8405;
-i__8401 = G__8406;
+var G__8482 = seq__8477;
+var G__8483 = chunk__8478;
+var G__8484 = count__8479;
+var G__8485 = (i__8480 + 1);
+seq__8477 = G__8482;
+chunk__8478 = G__8483;
+count__8479 = G__8484;
+i__8480 = G__8485;
 continue;
 }
 } else
-{var temp__4092__auto__ = cljs.core.seq.call(null,seq__8398);if(temp__4092__auto__)
-{var seq__8398__$1 = temp__4092__auto__;if(cljs.core.chunked_seq_QMARK_.call(null,seq__8398__$1))
-{var c__7497__auto__ = cljs.core.chunk_first.call(null,seq__8398__$1);{
-var G__8407 = cljs.core.chunk_rest.call(null,seq__8398__$1);
-var G__8408 = c__7497__auto__;
-var G__8409 = cljs.core.count.call(null,c__7497__auto__);
-var G__8410 = 0;
-seq__8398 = G__8407;
-chunk__8399 = G__8408;
-count__8400 = G__8409;
-i__8401 = G__8410;
+{var temp__4092__auto__ = cljs.core.seq.call(null,seq__8477);if(temp__4092__auto__)
+{var seq__8477__$1 = temp__4092__auto__;if(cljs.core.chunked_seq_QMARK_.call(null,seq__8477__$1))
+{var c__7497__auto__ = cljs.core.chunk_first.call(null,seq__8477__$1);{
+var G__8486 = cljs.core.chunk_rest.call(null,seq__8477__$1);
+var G__8487 = c__7497__auto__;
+var G__8488 = cljs.core.count.call(null,c__7497__auto__);
+var G__8489 = 0;
+seq__8477 = G__8486;
+chunk__8478 = G__8487;
+count__8479 = G__8488;
+i__8480 = G__8489;
 continue;
 }
 } else
-{var kywrd = cljs.core.first.call(null,seq__8398__$1);lt.plugins.mpc.mk_cmd.call(null,kywrd);
+{var kywrd = cljs.core.first.call(null,seq__8477__$1);lt.plugins.mpc.mk_cmd.call(null,mpc_path,kywrd);
 {
-var G__8411 = cljs.core.next.call(null,seq__8398__$1);
-var G__8412 = null;
-var G__8413 = 0;
-var G__8414 = 0;
-seq__8398 = G__8411;
-chunk__8399 = G__8412;
-count__8400 = G__8413;
-i__8401 = G__8414;
+var G__8490 = cljs.core.next.call(null,seq__8477__$1);
+var G__8491 = null;
+var G__8492 = 0;
+var G__8493 = 0;
+seq__8477 = G__8490;
+chunk__8478 = G__8491;
+count__8479 = G__8492;
+i__8480 = G__8493;
 continue;
 }
 }
@@ -88,7 +95,7 @@ continue;
 break;
 }
 });
-lt.plugins.mpc.main_.call(null);
+lt.object.behavior_STAR_.call(null,new cljs.core.Keyword("lt.plugins.mpc","mpc-init","lt.plugins.mpc/mpc-init",2904919510),new cljs.core.Keyword(null,"reaction","reaction",4441361819),lt.plugins.mpc.__BEH__mpc_init,new cljs.core.Keyword(null,"desc","desc",1016984067),"Init mpc with your beloved commands, ie: \"/usr/bin/mpc\" [:play :pause :toggle :next :prev :status :update]",new cljs.core.Keyword(null,"triggers","triggers",2516997421),new cljs.core.PersistentHashSet(null, new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"object.instant","object.instant",773332388),null], null), null));
 }
 
 //# sourceMappingURL=mpc_compiled.js.map
--- src/lt/plugins/mpc.cljs
+++ src/lt/plugins/mpc.cljs
@@ -2,42 +2,63 @@
   (:require [lt.object :as object]
             [lt.objs.tabs :as tabs]
             [lt.objs.proc :as proc]
+            [lt.objs.notifos :as notifos]
             [lt.objs.command :as cmd])
-  (:require-macros [lt.macros :refer [defui behavior]]))
+  (:require-macros [lt.macros :refer [behavior]]))
+
+
+
+(behavior ::shell-mpc.out
+          :desc "When mpc command is executed, show its out"
+          :triggers #{:proc.out}
+          :reaction (fn [ obj data ]
+                      (notifos/set-msg! (.toString data))))
+
+
+(def shell-mpc-out ;shell out object
+  (object/create
+   (object/object*
+    ::shell-mpc-out
+    :tags [:shell-mpc-out]
+    :behaviors [::shell-mpc.out])))
+
 
 
 (defn shell-mpc
   "Pass commands to shell mpc"
-  [ & cmds ]
+  [ mpc-path & cmds ]
   (partial proc/exec
-    {:command "mpc" :args cmds}))
+    {:command mpc-path
+     :args    cmds
+     :obj     shell-mpc-out}))
+
 
 
 (defn- mk-key
   "Make local keyword from string"
   [n]
-  (keyword
-   "lt.plugins.mpc"
-   (str "mpc-" n)))
+  (keyword (str *ns*) (str "mpc-" n)))
+
 
 
 (defn- mk-cmd
-  "Make LT commad that is visible "
-  [kywrd]
+  "Make LT commad that is visible for LT"
+  [mpc-path kywrd]
   (let [str-kywrd (name kywrd)
         new-kywrd (mk-key str-kywrd)
         desc      (str "mpc: " str-kywrd)
-        shell     (shell-mpc str-kywrd)]
+        shell     (shell-mpc mpc-path str-kywrd)]
     (cmd/command
      {:command  new-kywrd
       :desc     desc
       :exec     shell})))
 
-(defn main- []
-  ; make LT command for those keywords
-  (doseq [kywrd [:play :pause :stop :toggle :next :prev]]
-    (mk-cmd kywrd)))
 
 
-(main-)
+(behavior ::mpc-init
+          :triggers #{:object.instant}
+          :desc "Init mpc with your beloved commands, ie: \"/usr/bin/mpc\" [:play :pause :toggle :next :prev :status :update]"
+          :reaction (fn [this mpc-path commands]
+                      (doseq [kywrd commands]
+                        (mk-cmd mpc-path kywrd))))
 
